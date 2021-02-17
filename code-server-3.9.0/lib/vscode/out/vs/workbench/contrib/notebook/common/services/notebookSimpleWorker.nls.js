3d5ac3f553d135104da8611679caafa10e5fdd00
selodev <selahattin47usa@hotmail.com>
2021-02-17 04:37:18 +0000

    work in progress

--- code-server-3.9.0/lib/vscode/out/vs/workbench/contrib/notebook/common/services/notebookSimpleWorker.nls.js
+++
@@ -1,5 +0,0 @@
-/*!--------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-define("vs/workbench/contrib/notebook/common/services/notebookSimpleWorker.nls",{"vs/base/common/codicons":["Icon for drop down buttons."]});
-//# sourceMappingURL=notebookSimpleWorker.nls.js.map
